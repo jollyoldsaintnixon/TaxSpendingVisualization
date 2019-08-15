c0c469f8e9217aa51b4fff5129fba04c72adeff8
jOSN <appapplication@protonmail.com>
2019-08-14 22:15:45 -0400

    Add NC data

--- src/components/pie_chart.js
+++ src/components/pie_chart.js
@@ -1,75 +1,93 @@
-// import bf_array from './federal_object_array'
 
+let TOTAL = 0;
 // CIRCLE TIME BABY
-const section = document.createElement('section')
 // margin and radius
-const margin = {top: 20, right: 20, bottom: 20, left: 20},
-    height = 500 - margin.top - margin.bottom,
-    width = 500 - margin.left - margin.right,
+const margin = {top: 200, right: 200, bottom: 200, left: 200},
+    height = 1000 - margin.top - margin.bottom,
+    width = 1000 - margin.left - margin.right,
     radius = width/2;
-
-const colors = d3.scaleOrdinal(d3.schemeDark2);
-
-// arc generator
-const arc = d3.arc()
+    
+    const colors = d3.scaleOrdinal(d3.schemeDark2);
+    
+    // arc generator
+    const arc = d3.arc()
     .outerRadius(radius - 10)
     // .innerRadius(0); // for circle
-    .innerRadius(radius - 70) // for donut
-
-const lableArc = d3.arc()
+    .innerRadius(radius - 100) // for donut
+    
+    const lableArc = d3.arc()
     .outerRadius(radius - 50)
     .innerRadius(radius - 50);
-
-// pie generator
-const pie = d3.pie()
-    .sort(null)
+    
+    // pie generator
+    const pie = d3.pie()
+    // .sort(null)
     .value(d => d.amount);
-
-// define svg 
-const svg = d3.select("svg")
+    
+    // define svg 
+    const svg = d3.select("svg")
     .attr("width", width)
     .attr("height", height)
+    .style("display", "flex")
+    .style("background", "yellow")
     .append("g")
     .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
-
-// import data
-d3.csv("./src/assets/data/california_2019.csv").then(function(data) {   
-    // parse
-    data.forEach(d => {
-        d.sector = d.StateAgencies;
-        d.amount = +d.TotalStateFunds;
-    })
-    // append g elements arc
-    const g = svg.selectAll(".arc")
+    
+    // import data
+    d3.csv("./src/assets/data/california_2019.csv").then(function(data) {   
+        // parse
+        
+        data.forEach(d => {
+            d.sector = d.StateAgencies;
+            d.amount = +d.TotalStateFunds;
+            TOTAL += +d.TotalStateFunds;
+        })
+        
+        console.log(TOTAL)
+        // append g elements arc
+        const g = svg.selectAll(".arc")
         .data(pie(data))
         .enter().append("g")
         .attr("class", "arc");
-
+        
         // append the path of the arc
-    g.append("path")
+        g.append("path")
         .attr("d", arc)
         .style("fill", d => colors(d.data.sector))
-
-    g.append("text")
+        .on("mouseover", ele => {
+            console.log(ele)
+            h1.text(ele.data.sector + " accounts for $" + ele.data.amount + " out of $" + TOTAL)
+            h2.text("This is " + String((ele.data.amount / TOTAL) * 100).slice(0, 5) + "% of the total")
+        })
+        .on("mouseout", ele => {
+            h1.text("California's total budget for 2019 was $" + TOTAL)
+            h2.text("")
+        });
+        
+        g.append("text")
         .style("fill", d => "pink")
         // .ease(d3.easeLinear)
         // .duration(2000)
         .attr("transform", d => {return "translate(" + lableArc.centroid(d) + ")";})
         .attr("dy", ".5em")
         .text(d => d.data.sector)
-        .style("display", "block")
-}).catch(error => { if (error) throw error})
-
-
-    // .ease(d3.easeLinear)
-    // .duration(2000)
-    // .attrTween("d", pieTween)
-
-// append labels
+        .style("width", "fit-content")
+        .style("z-index", "1")
+        // .ease(d3.easeLinear)
+        // .duration(2000)
+        // .attrTween("d", pieTween)
+    }).catch(error => { if (error) throw error})
     
-const pieTween = b => {
-    b.innerRadius = 0;
-    const i = d3.interpolate({startAngle: 0, endAndle: 0}, b)
+    
+const h1 = d3.select("main")
+    .append("h1")
+
+const h2 = d3.select("main")
+    .append("h2")
+    
+    const pieTween = b => {
+        b.innerRadius = 0;
+    const i = d3.interpolate({startAngle: 0, endAngle: 0}, b)
     return (t) => { return arc(i(t))}
 }    
 // export default svg
